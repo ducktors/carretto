685416410bc35dd8a10edd66c0ba7a750d6dc0a5
Alessandro Magionami <alessandro.magionami@gmail.com>
2022-12-14 22:30:49 +0100

    feat(main-loader): make loader an event emitter

---
+++ packages/main-loader/lib/events.ts
@@ -0,0 +1,6 @@
+export const Events = {
+  LoadStarted: 'carretto_load_started',
+  LoadError: 'carretto_load_error',
+}
+
+export type Events = typeof Events
--- packages/main-loader/lib/main-loader.ts
+++ packages/main-loader/lib/main-loader.ts
@@ -1,14 +1,17 @@
-import DataLoader, { BatchLoadFn } from "dataloader";
+import DataLoader, { BatchLoadFn } from "dataloader"
+import { EventEmitter } from 'node:events'
 
 import { hash } from "./hash";
 import type { Key } from "./key";
 import { createQueriesMapFactory } from './create-queries-map-factory'
 import { MergeProjectionFn } from "./merge-projection-fn";
+import { Events } from "./events";
 
-export abstract class MainLoader<T, Q extends object, P> {
+export abstract class MainLoader<T, Q extends object, P> extends EventEmitter {
 	protected loader: DataLoader<Key<Q, P>, T>;
 
 	constructor() {
+		super()
 		this.loader = new DataLoader<Key<Q, P>, T, string>(
 			this.batchLoadFn.bind(this) as BatchLoadFn<Key<Q, P>, T>,
 			{
@@ -18,23 +21,19 @@ export abstract class MainLoader<T, Q extends object, P> {
 	}
 
 	public async load(key: Key<Q, P>): Promise<T | null> {
-		this.onLoad(key)
+		this.emit(Events.LoadStarted, { key })
 		return this.loader.load(key)
 	}
 
 	public async loadMany(key: Key<Q, P>): Promise<T[]> {
 		if (!Object.hasOwn(key, 'skip')) {
-			const error = new Error('Missing "skip" property in key')
-			this.onError(error, key)
-			throw error
+			throw new Error('Missing "skip" property in key')
 		}
 		if (!Object.hasOwn(key, 'limit')) {
-			const error = new Error('Missing "limit" property in key')
-			this.onError(error, key)
-			throw error
+			throw new Error('Missing "limit" property in key')
 		}
 
-		this.onLoad(key)
+		this.emit(Events.LoadStarted, { key })
 		return this.loader.load(key) as Promise<T[]>
 	}
 
@@ -56,10 +55,6 @@ export abstract class MainLoader<T, Q extends object, P> {
 	protected cacheKeyFn(key: Key<Q, P>) {
 		return hash(key);
 	}
-
-	protected onLoad(key: Key<Q, P>) {}
-
-	protected onError(error: Error, key: Key<Q, P>) {}
 
 	protected abstract execute(key: Key<Q, P>): Promise<T | T[] | null>;
 	protected abstract mergeProjection: MergeProjectionFn<P, P>;
