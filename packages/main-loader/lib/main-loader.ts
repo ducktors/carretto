008e2d9ed82c6215e16d0e1ab882d20562104e51
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-01-16 20:26:35 +0100

    feat(main-loader): revert to hook system

--- packages/main-loader/lib/main-loader.ts
+++ packages/main-loader/lib/main-loader.ts
@@ -1,16 +1,13 @@
 import DataLoader, { BatchLoadFn } from "dataloader"
-import { EventEmitter } from 'node:events'
 
 import { hash } from "./hash";
 import type { Key } from "./key";
 import { createQueriesMapFactory } from './create-queries-map-factory'
-import { Events } from "./events";
 
-export abstract class MainLoader<T, Q extends object> extends EventEmitter {
+export abstract class MainLoader<T, Q extends object> {
 	protected loader: DataLoader<Key<Q>, T>;
 
 	constructor() {
-		super()
 		this.loader = new DataLoader<Key<Q>, T, string>(
 			this.batchLoadFn.bind(this) as BatchLoadFn<Key<Q>, T>,
 			{
@@ -20,19 +17,23 @@ export abstract class MainLoader<T, Q extends object> extends EventEmitter {
 	}
 
 	public async load(key: Key<Q>): Promise<T | null> {
-		this.emit(Events.LoadStarted, { key })
+		this.onLoad(key)
 		return this.loader.load(key)
 	}
 
 	public async loadMany(key: Key<Q>): Promise<T[]> {
 		if (!Object.hasOwn(key, 'skip')) {
-			throw new Error('Missing "skip" property in key')
+			const error = new Error('Missing "skip" property in key')
+			this.onError(error, key)
+			throw error
 		}
 		if (!Object.hasOwn(key, 'limit')) {
-			throw new Error('Missing "limit" property in key')
+			const error = new Error('Missing "limit" property in key')
+			this.onError(error, key)
+			throw error
 		}
 
-		this.emit(Events.LoadStarted, { key })
+		this.onLoad(key)
 		return this.loader.load(key) as Promise<T[]>
 	}
 
@@ -54,6 +55,10 @@ export abstract class MainLoader<T, Q extends object> extends EventEmitter {
 	protected cacheKeyFn(key: Key<Q>) {
 		return hash(key);
 	}
+
+	protected onLoad(key: Key<Q>) {}
+
+	protected onError(error: Error, key: Key<Q>) {}
 
 	protected abstract execute(key: Key<Q>): Promise<T | T[] | null>;
 }
