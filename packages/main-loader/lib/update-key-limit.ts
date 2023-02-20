7533f001aac6869aec42c173a1e6da6578cd8fc8
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-02-20 17:43:55 +0100

    chore(main-loader): remove unused code

--- packages/main-loader/lib/update-key-limit.ts
+++ packages/main-loader/lib/update-key-limit.ts
@@ -1,15 +1,9 @@
 import { Key } from './key';
 
 export function updateKeyLimit<Q extends object>(previous: Key<Q>, current: Key<Q>) {
-  if (!(previous.limit && current.limit) || previous.limit === 0 || current.limit === 0) {
+  if (!(previous.limit && current.limit ) || previous.limit === 0 || current.limit === 0) {
     return 0;
   }
 
-  if (!current.limit && previous.limit) {
-    return previous.limit;
-  }
-  if (!previous.limit && current.limit) {
-    return current.limit;
-  }
   return Math.max(previous.limit, current.limit);
 }
--- packages/main-loader/lib/update-key-skip.ts
+++ packages/main-loader/lib/update-key-skip.ts
@@ -5,11 +5,5 @@ export function updateKeySkip<Q extends object>(previous: Key<Q>, current: Key<Q
     return 0;
   }
 
-  if (!current.skip && previous.skip) {
-    return previous.skip;
-  }
-  if (!previous.skip && current.skip) {
-    return current.skip;
-  }
   return Math.min(previous.skip, current.skip);
 }
