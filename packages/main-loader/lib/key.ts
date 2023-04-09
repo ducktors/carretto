3b36fb633938fabb124e86cae3dc742e04cde11a
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-04-09 19:26:30 +0200

    feat(main-loader): support sort

--- packages/main-loader/lib/key.ts
+++ packages/main-loader/lib/key.ts
@@ -1,8 +1,9 @@
 import { Projection } from '@carretto/projection';
 
-export interface Key<TQuery extends object> {
+export interface Key<TQuery extends object, TSort = Record<string, 1 | -1>> {
   query: TQuery;
   projection: Projection;
   skip?: number;
   limit?: number;
+  sort?: TSort;
 }
