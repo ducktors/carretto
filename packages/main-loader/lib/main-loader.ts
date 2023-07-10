f07b9558a6ac4d86dd671127d6a2d62b19dd4f9a
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-07-10 18:12:44 +0200

    feat(main-loader): optimize memory usage for createQueriesMap in class

--- packages/main-loader/lib/main-loader.ts
+++ packages/main-loader/lib/main-loader.ts
@@ -1,32 +1,44 @@
-import DataLoader, { BatchLoadFn } from 'dataloader';
+import DataLoader, { BatchLoadFn } from "dataloader";
 
-import { hash } from './hash';
-import type { Key } from './key';
-import { createQueriesMapFactory } from './create-queries-map-factory';
+import { hash } from "./hash";
+import type { Key } from "./key";
+import { createQueriesMapFactory } from "./create-queries-map-factory";
 
-export abstract class MainLoader<TReturn, TQuery extends object, TLoadKey extends Pick<Key<TQuery>, 'query' | 'skip' | 'limit'> = Key<TQuery>> {
+const createQueriesMap = createQueriesMapFactory();
+
+export abstract class MainLoader<
+  TReturn,
+  TQuery extends object,
+  TLoadKey extends Pick<Key<TQuery>, "query" | "skip" | "limit"> = Key<TQuery>
+> {
   protected loader: DataLoader<Key<TQuery>, TReturn>;
-  protected createQueriesMap;
 
   constructor() {
     this.loader = new DataLoader<Key<TQuery>, TReturn, string>(
       this.batchLoadFn.bind(this) as BatchLoadFn<Key<TQuery>, TReturn>,
       {
         cacheKeyFn: this.cacheKeyFn,
-      },
+      }
     );
-    this.createQueriesMap = createQueriesMapFactory()
   }
 
-  public async load<U extends TReturn = TReturn>(key: TLoadKey): Promise<U | null> {
+  public async load<U extends TReturn = TReturn>(
+    key: TLoadKey
+  ): Promise<U | null> {
     const internalKey = this.preLoad(key);
     return this.loader.load(internalKey) as Promise<U | null>;
   }
 
-  public async loadMany<U extends TReturn = TReturn>(key: TLoadKey): Promise<U[]> {
+  public async loadMany<U extends TReturn = TReturn>(
+    key: TLoadKey
+  ): Promise<U[]> {
     const internalKey = this.preLoad(key);
-    const { skip, limit } = key
-    return this.loader.load({ skip: skip ?? 0, limit: limit ?? 0, ...internalKey }) as Promise<U[]>;
+    const { skip, limit } = key;
+    return this.loader.load({
+      skip: skip ?? 0,
+      limit: limit ?? 0,
+      ...internalKey,
+    }) as Promise<U[]>;
   }
 
   protected async batchLoadFn(keys: readonly Key<TQuery>[]) {
@@ -48,9 +60,18 @@ export abstract class MainLoader<TReturn, TQuery extends object, TLoadKey extend
     return hash(key);
   }
 
-  protected preLoad(key: TLoadKey): Key<TQuery> { return key as unknown as Key<TQuery> }
+  private createQueriesMap(keys: readonly Key<TQuery>[]) {
+    return createQueriesMap(keys);
+  }
 
-  protected onError(error: Error, key: Key<TQuery>) {}
+  protected preLoad(key: TLoadKey): Key<TQuery> {
+    return key as unknown as Key<TQuery>;
+  }
 
-  protected abstract execute(key: Key<TQuery>, options?: unknown): Promise<TReturn | TReturn[] | null>;
+  protected onError(error: Error, key: Key<TQuery>) { }
+
+  protected abstract execute(
+    key: Key<TQuery>,
+    options?: unknown
+  ): Promise<TReturn | TReturn[] | null>;
 }
