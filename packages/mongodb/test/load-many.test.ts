2c57778868e0df554003202e8452b21e67adbc92
Alessandro Magionami <alessandro.magionami@gmail.com>
2024-08-09 12:07:55 +0200

    chore(mongodb): fix tests

--- packages/mongodb/test/load-many.test.ts
+++ packages/mongodb/test/load-many.test.ts
@@ -1,20 +1,21 @@
 import assert from 'node:assert';
 import { after, before, test } from 'node:test';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 import { Document, MongoClient, ObjectId, WithId } from 'mongodb';
-import { setup, teardown } from 'vitest-mongodb';
 import { DataloaderMongoDB } from '../lib';
 
 let client: MongoClient;
+let mongod: MongoMemoryServer;
 before(async () => {
-  await setup();
-  client = new MongoClient(global.__MONGO_URI__);
+  mongod = await MongoMemoryServer.create();
+  client = new MongoClient(mongod.getUri());
   await client.connect();
 });
 
 after(async () => {
   await client.close();
-  await teardown();
+  await mongod.stop();
 });
 
 test('should aggregate same queries projections and skip and limit', async (t) => {
